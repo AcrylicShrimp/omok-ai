c784d87507b306b79ebbc4c4fc65bc9f49f92189
AcrylicShrimp <led789zxpp@naver.com>
2020-03-04 22:30:46 +0900

    Modified state input format.

--- src/policy_net.py
+++ src/policy_net.py
@@ -6,11 +6,15 @@ import torch.nn.functional as F
 class PolicyNet(nn.Module):
     def __init__(self):
         super(PolicyNet, self).__init__()
+        self.fc1 = nn.Linear(730, 729)
         self.conv1 = nn.Conv2d(9, 32, 5)   # >> 5x5
         self.conv2 = nn.Conv2d(32, 64, 3)  # >> 3x3
         self.conv3 = nn.Conv2d(64, 81, 3)  # >> 1x1
 
         def forward(self, x):
+            x = self.fc1(x)
+            x = F.leaky_relu(x)
+            x = x.view(9, 9, 9)
             x = self.conv1(x)
             x = F.leaky_relu(x)
             x = self.conv2(x)
--- src/value_net.py
+++ src/value_net.py
@@ -6,11 +6,15 @@ import torch.nn.functional as F
 class ValueNet(nn.Module):
     def __init__(self):
         super(ValueNet, self).__init__()
+        self.fc1 = nn.Linear(730, 729)
         self.conv1 = nn.Conv2d(9, 32, 5)   # >> 5x5
         self.conv2 = nn.Conv2d(32, 64, 3)  # >> 3x3
         self.conv3 = nn.Conv2d(64, 1, 3)
 
         def forward(self, x):
+            x = self.fc1(x)
+            x = F.leaky_relu(x)
+            x = x.view(9, 9, 9)
             x = self.conv1(x)
             x = F.leaky_relu(x)
             x = self.conv2(x)
