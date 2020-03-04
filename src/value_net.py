678dd7ce3c5618518f4537c31a2d51788a27361c
AcrylicShrimp <led789zxpp@naver.com>
2020-03-04 22:22:51 +0900

    Implemented nets.

---
+++ src/value_net.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ValueNet(nn.Module):
+    def __init__(self):
+        super(ValueNet, self).__init__()
+        self.conv1 = nn.Conv2d(9, 32, 5)   # >> 5x5
+        self.conv2 = nn.Conv2d(32, 64, 3)  # >> 3x3
+        self.conv3 = nn.Conv2d(64, 1, 3)
+
+        def forward(self, x):
+            x = self.conv1(x)
+            x = F.leaky_relu(x)
+            x = self.conv2(x)
+            x = F.leaky_relu(x)
+            x = self.conv3(x)
+            x = F.leaky_relu(x)
+            output = x.flatten()
+            return output
