f8bc613e87e87e3190fe39f27714911c807b3b1b
AcrylicShrimp <led789zxpp@naver.com>
2023-05-01 02:25:55 +0900

    fix: correct transition logic.

--- mcts/src/lib.rs
+++ mcts/src/lib.rs
@@ -35,23 +35,43 @@ impl<S> MCTS<S> {
     }
 
     pub fn transition(&mut self, children_index: usize) {
+        let allocator = &mut self.allocator.lock();
+
+        let new_root = {
         let root = unsafe { &mut *self.root };
         let root_children = root.children.read();
-        let allocator = &mut self.allocator.lock();
 
         for index in 0..root_children.len() {
             if index == children_index {
                 continue;
             }
 
-            let child = unsafe { &mut *(root_children[index] as *mut Node<S>) };
-            allocator.deallocate(child);
+                dealloc_node(root_children[index].ptr as *mut Node<S>, allocator);
         }
 
-        let new_root = unsafe { &mut *(root_children[children_index] as *mut Node<S>) };
+            let new_root = unsafe { &mut *(root_children[children_index].ptr as *mut Node<S>) };
         new_root.parent = None;
+            new_root as *mut Node<S>
+        };
 
         allocator.deallocate(self.root);
         self.root = new_root;
     }
 }
+
+fn dealloc_node<S>(ptr: *mut Node<S>, allocator: &mut BumpAllocator<Node<S>>)
+where
+    S: State,
+{
+    {
+        let node = unsafe { &mut *ptr };
+        let children = node.children.read();
+        for child in children.iter() {
+            dealloc_node(child.ptr as *mut Node<S>, allocator);
+        }
+    }
+    allocator.deallocate(ptr);
+}
+
+unsafe impl<S> Send for MCTS<S> where S: State {}
+unsafe impl<S> Sync for MCTS<S> where S: State {}
