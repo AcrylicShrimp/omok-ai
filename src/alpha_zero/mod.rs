85eaa0172eba0cf230cdac8d38ba1daeefafdd64
AcrylicShrimp <led789zxpp@naver.com>
2023-05-05 01:57:06 +0900

    refactor: make code readable.

--- src/alpha_zero/mod.rs
+++ src/alpha_zero/mod.rs
@@ -39,16 +39,26 @@ pub struct Train {
     pub replay_memory: VecDeque<Transition>,
 }
 
+macro_rules! batched {
+    ($expr:expr) => {
+        if MCTSExecutor::NN_EVALUATION_BATCH_SIZE < $expr {
+            $expr / MCTSExecutor::NN_EVALUATION_BATCH_SIZE
+        } else {
+            1
+        }
+    };
+}
+
 impl Train {
     pub const MODEL_NAME: &'static str = "alpha-zero";
 
     pub const REPLAY_MEMORY_SIZE: usize = 5_000;
     pub const EPISODE_COUNT: usize = 50;
-    pub const EVALUATE_COUNT: usize = 1600 / MCTSExecutor::NN_EVALUATION_BATCH_SIZE;
+    pub const EVALUATE_COUNT: usize = batched!(1600);
     pub const TRAINING_COUNT: usize = 100;
     pub const BATCH_SIZE: usize = 64;
 
-    pub const TEST_EVALUATE_COUNT: usize = 320 / MCTSExecutor::NN_EVALUATION_BATCH_SIZE;
+    pub const TEST_EVALUATE_COUNT: usize = batched!(320);
 
     pub const TEMPERATURE: f32 = 1.0;
     pub const TEMPERATURE_THRESHOLD: usize = 30;
